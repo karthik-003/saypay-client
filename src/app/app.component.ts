047a4fec31a03799981e8aeeef759435c5f09bff
Karthik Viswanadha <kviswanadha@altimetrik.com>
2021-04-19 23:06:58 +0530

    # commit to test voice output
    Signed-off-by: Karthik Viswanadha <kviswanadha@altimetrik.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -86,6 +86,7 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
 	// New comment. 
 	// another new comment
+	//voice test commit.
     this.nav.setRoot(page.component);
   }
 }
