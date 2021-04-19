f9be0ab130a1757e229eec2ece2552d40742f4a8
Karthik Viswanadha <kviswanadha@altimetrik.com>
2021-04-19 20:25:34 +0530

    simple test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,6 +84,7 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
+	// New comment. 
     this.nav.setRoot(page.component);
   }
 }
