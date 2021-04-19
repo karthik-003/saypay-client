89fc7623dff5ced5ef851d29d9a89cb67ca3b707
Karthik Viswanadha <kviswanadha@altimetrik.com>
2021-04-19 23:08:24 +0530

    # Removing unwanted comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,9 +84,7 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-	// New comment. 
-	// another new comment
-	//voice test commit.
+	
     this.nav.setRoot(page.component);
   }
 }
