1b0dcb6b58367b6889ec05b25d50d02f26c5d411
Karthik Viswanadha <kviswanadha@altimetrik.com>
2021-04-20 10:55:51 +0530

    # Testing with pyTTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,7 +84,7 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-	// critical code
+	// critical code - dont touch.
     this.nav.setRoot(page.component);
   }
 }
