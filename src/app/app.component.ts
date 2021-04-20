4c45ddf426044439ac8eb477bd3480183ed94dbc
Karthik Viswanadha <kviswanadha@altimetrik.com>
2021-04-20 10:56:41 +0530

    # Adding loggers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -85,6 +85,7 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
 	// critical code - dont touch.
+	console.log('Sample log.')
     this.nav.setRoot(page.component);
   }
 }
